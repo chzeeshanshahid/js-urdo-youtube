d1337fed0256518225239dadd382d4bd7c4def42
chzeeshanshahid <168276528+chzeeshanshahid@users.noreply.github.com>
2024-07-31 05:05:21 +0000

    Comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);      [
+// console.log(2 < 1);            
+// console.log(2 >= 1);         simple conparision
+// console.log(2 != 1)
+// console.log(2 == 1);     ]
+
+
+// console.log("2" > 1);         [
+// console.log("02" > 1);
+                               
+// console.log(null > 0);           
+// console.log(null == 0);       avoid this comparision 
+// console.log(null >= 0);      
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);      ]
+                               
+
+console.log("2" === 2);
